735407272b70369ff5d412585349f0cb82fb9193
elizabethLomb <elizabeth.lombardi7@gmail.com>
2019-12-02 17:51:50 +0100

    Monstando estructura y configurando seeds definir contenido de todos los models

---
+++ bin/seeds.js
@@ -0,0 +1,5 @@
+require('../config/db.config')
+const faker = require('faker')
+
+//hay que llamar a todos los modelos aquí
+
---
+++ models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+// const SALT_WORK_FACTOR = 10;
+
+const userShema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [3, 'Name needs at last 8 chars'],
+    trim: true
+  },
+  lastname: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [3, 'Name needs at last 8 chars'],
+    trim: true
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [EMAIL_PATTERN, 'Email is invalid']
+  },
+  about: {
+    type: String
+  },
+  city: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password needs at last 8 chars']
+  },
+  avatar: {
+    type: String,
+  },
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
